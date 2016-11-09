0918ae6874dd39d8281a1ba67b2a0c77bffc8612
Turbo Fredriksson <turbo@bayour.com>
2016-11-09 10:14:31 +0000

    Initial commit.
    Work by: This is the original, first example script that John Engelman
             published on the 'Net on July 7, 2015.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Lambda-AWS-AutoScalingGroups-Route53
-Updating Route53 DNS for AutoScalingGroup using Lambda
+AWS Tricks: Updating Route53 DNS for AutoScalingGroup using Lambda
+
+This is the works of John Engelman, which he published on the 'Net on July 7, 2015.
+
+The original is located at https://objectpartners.com/2015/07/07/aws-tricks-updating-route53-dns-for-autoscalinggroup-using-lambda/
+and this is just my way of keeping track of the changes of that
+blog posting.
+
+
+Each commit will have a "Work by" at the end to indicate who did
+what.
---
+++ asg_lambda.js
@@ -0,0 +1,108 @@
+var AWS = require('aws-sdk');
+var async = require('async');
+
+exports.handler = function (event, context) {
+  var asg_msg = JSON.parse(event.Records[0].Sns.Message);
+  var asg_name = asg_msg.AutoScalingGroupName;
+  var instance_id = asg_msg.EC2InstanceId;
+  var asg_event = asg_msg.Event;
+
+  console.log(asg_event);
+  if (asg_event === "autoscaling:EC2_INSTANCE_LAUNCH" || asg_event === "autoscaling:EC2_INSTANCE_TERMINATE") {
+    console.log("Handling Launch/Terminate Event for " + asg_name);
+    var autoscaling = new AWS.AutoScaling({region: 'us-east-1'});
+    var ec2 = new AWS.EC2({region: 'us-east-1'});
+    var route53 = new AWS.Route53();
+
+    async.waterfall([
+         function describeTags(next) {
+           console.log("Describing ASG Tags");
+           autoscaling.describeTags({
+               Filters: [
+                   {
+                     Name: "auto-scaling-group",
+                       Values: [
+                           asg_name
+                       ]
+                   },
+                   {
+                     Name: "key",
+                       Values: ['DomainMeta']
+                   }
+               ],
+               MaxRecords: 1
+           }, next);
+         },
+         function processTags(response, next) {
+           console.log("Processing ASG Tags");
+           console.log(response.Tags);
+           if (response.Tags.length == 0) {
+             next("ASG: " + asg_name + " does not define Route53 DomainMeta tag.");
+           }
+           var tokens = response.Tags[0].Value.split(':');
+           var route53Tags = {
+             HostedZoneId: tokens[0],
+             RecordName: tokens[1]
+           };
+           console.log(route53Tags);
+           next(null, route53Tags);
+         },
+         function retrieveASGInstances(route53Tags, next) {
+           console.log("Retrieving Instances in ASG");
+           autoscaling.describeAutoScalingGroups({
+               AutoScalingGroupNames: [asg_name],
+                 MaxRecords: 1
+                 }, function(err, data) {
+               next(err, route53Tags, data);
+           });
+         },
+         function retrieveInstanceIds(route53Tags, asgResponse, next) {
+           console.log(asgResponse.AutoScalingGroups[0]);
+           var instance_ids = asgResponse.AutoScalingGroups[0].Instances.map(function(instance) {
+               return instance.InstanceId
+           });
+           ec2.describeInstances({
+               DryRun: false,
+                 InstanceIds: instance_ids
+                 }, function(err, data) {
+               next(err, route53Tags, data);
+           });
+         },
+         function updateDNS(route53Tags, ec2Response, next) {
+           console.log(ec2Response.Reservations);
+           var resource_records = ec2Response.Reservations.map(function(reservation) {
+               return {
+                 Value: reservation.Instances[0].NetworkInterfaces[0].Association.PublicIp
+               };
+           });
+           console.log(resource_records);
+           route53.changeResourceRecordSets({
+               ChangeBatch: {
+                 Changes: [
+                     {
+                       Action: 'UPSERT',
+                         ResourceRecordSet: {
+                         Name: route53Tags.RecordName,
+                           Type: 'A',
+                           TTL: 10,
+                           ResourceRecords: resource_records
+                         }
+                     }
+                 ]
+               },
+               HostedZoneId: route53Tags.HostedZoneId
+           }, next);
+         }
+    ], function (err) {
+         if (err) {
+            console.error('Failed to process DNS updates for ASG event: ', err);
+         } else {
+            console.log("Successfully processed DNS updates for ASG event.");
+         }
+         context.done(err);
+    })
+  } else {
+    console.log("Unsupported ASG event: " + asg_name, asg_event);
+    context.done("Unsupported ASG event: " + asg_name, asg_event);
+  }
+};
