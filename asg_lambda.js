ee4b80b790dda7eac0de65bf578611a5da5329ef
Turbo Fredriksson <turbo@bayour.com>
2016-11-10 12:18:31 +0000

    Updates.
    Work by Turbo Fredriksson
    * Make region, do round-robin configurable at the top.
      + Add a 'do_debug' option which disables the actual
        R53 update(s).
        + Should probably put most of the current debug output
          inside such a check..
      + If we're not doing round-robin, don't get ASG instances,
        just information about the one in the SNS message.
        I.e., we're updating a record ONLY for that.
        NOTE: This will currently always overwrite the 'blabla-0'
              entry!!
    * Add a 'flow chart' of the function as a reminder.
      + Include the two TODOs about getting more R53 info.
    * JSON stringify all relevant log output to 'prettify' it.
    * The template for the individual forward/reverse entry is now
      always 'XX' (as in, prefixed with a 0 if counter < 10).
      This is the problem with the NOTE above. But this is where the
      two TODOs come in..

--- asg_lambda.js
+++ asg_lambda.js
@@ -1,5 +1,26 @@
 var AWS = require('aws-sdk');
 var async = require('async');
+var region = 'us-east-1';
+var do_round_robin = true;
+var do_debug = false;
+
+/*
+ * Flow:
+ * Get tags from ASG
+ * Process tags retreived
+ * Get instances in ASG (if do_round_robin=true)
+ * Get ID(s) of the instances retreived
+ * Get IP(s) of the instances retreived
+ * Sort IP(s) retreived
+ * Create reverse IP(s) of the IP(s) retreived
+ * Get hosted zone(s) from R53
+ * Get ID(s) for zone(s) retreived
+ + Get [all] zone A/PTR record(s) of the zone(s) retreived
+ + Sort out zone record(s) that matches 'DomainMeta'
+ * Figure out which reverse IP(s) belong to which R53 zone
+ * Change the reverse DNS zone(s) with the reverse IP(s) retreived
+ * Change the forward DNS zone(s) with the instance IP(s) retreived
+ */
 
 function normalizeIP(ip) {
     return ip.split("\.").map(function(s) {
@@ -26,10 +47,10 @@ exports.handler = function (event, context) {
 
   console.log(asg_event);
   if (asg_event === "autoscaling:EC2_INSTANCE_LAUNCH" || asg_event === "autoscaling:EC2_INSTANCE_TERMINATE") {
-    console.log("Handling Launch/Terminate Event for " + asg_name);
+    console.log(asg_msg.Description);
 
-    var autoscaling = new AWS.AutoScaling({region: 'us-east-1'});
-    var ec2 = new AWS.EC2({region: 'us-east-1'});
+    var autoscaling = new AWS.AutoScaling({region: region});
+    var ec2 = new AWS.EC2({region: region});
     var route53 = new AWS.Route53();
 
     async.waterfall([
@@ -51,7 +72,7 @@ exports.handler = function (event, context) {
          },
          function processTags(response, next) {
            console.log("Processing ASG Tags");
-           console.log(response.Tags);
+           console.log(JSON.stringify(response.Tags, null, 2));
            if (response.Tags.length === 0) {
              return next("ASG: " + asg_name + " does not define Route53 DomainMeta tag.");
            }
@@ -61,34 +82,44 @@ exports.handler = function (event, context) {
              RecordName: tokens[1],
              DomainName: tokens[2]
            };
-           console.log(route53Tags);
+           console.log(JSON.stringify(route53Tags, null, 2));
            next(null, route53Tags);
          },
          function retrieveASGInstances(route53Tags, next) {
-           console.log("Retrieving Instances in ASG");
-           autoscaling.describeAutoScalingGroups({
-               AutoScalingGroupNames: [asg_name],
-               MaxRecords: 1
-           }, function(err, data) {
-               console.log(data);
-               next(err, route53Tags, data);
-           });
+           if (do_round_robin) {
+             console.log("Retrieving Instances in ASG");
+             autoscaling.describeAutoScalingGroups({
+                 AutoScalingGroupNames: [asg_name],
+                 MaxRecords: 1
+             }, function(err, data) {
+                 console.log(JSON.stringify(data, null, 2));
+                 next(err, route53Tags, data);
+             });
+           } else {
+             next(null, route53Tags, null);
+           }
          },
          function retrieveInstanceIds(route53Tags, asgResponse, next) {
-           console.log("Retrieving Instance IDs in ASG");
-           console.log(asgResponse.AutoScalingGroups[0]);
-           var instance_ids = asgResponse.AutoScalingGroups[0].Instances.map(function(instance) {
+           console.log("Retrieving Instance ID(s) in ASG");
+           var instance_ids
+           if (do_round_robin) {
+             console.log(JSON.stringify(asgResponse.AutoScalingGroups[0], null, 2));
+             instance_ids = asgResponse.AutoScalingGroups[0].Instances.map(function(instance) {
                return instance.InstanceId;
-           });
+             });
+           } else {
+             instance_ids = [ instance_id ];
+           }
            ec2.describeInstances({
                DryRun: false,
                InstanceIds: instance_ids
            }, function(err, data) {
-               console.log(data);
                next(err, route53Tags, data);
            });
          },
          function setupIpAddresses(route53Tags, ec2Response, next) {
+           console.log("Getting instance(s) IP addresses");
+           console.log(JSON.stringify(ec2Response, null, 2));
            var resource_records = ec2Response.Reservations.map(function(reservation) {
                var instance = reservation.Instances[0];
                return instance.PublicIpAddress ? {
@@ -99,6 +130,8 @@ exports.handler = function (event, context) {
            }).filter(function(ip) {
                return ip.Value !== undefined;
            });
+           console.log("Resource records");
+           console.log(JSON.stringify(resource_records, null, 2));
            next(null, route53Tags, resource_records);
          },
          function normalizeIPs(route53Tags, resource_records, next) {
@@ -121,9 +154,9 @@ exports.handler = function (event, context) {
          },
          function retrieveZoneIds(route53Tags, resource_records, reverse_records, r53Response, next) {
              console.log("Reverse records:");
-             console.log(reverse_records);
+             console.log(JSON.stringify(reverse_records, null, 2));
              console.log("Hosted zones:");
-             console.log(r53Response.HostedZones);
+             console.log(JSON.stringify(r53Response.HostedZones, null, 2));
              var zone_ids = r53Response.HostedZones.map(function(zone) {
                  return {
                      Id: zone.Id.split("/")[2],
@@ -131,7 +164,7 @@ exports.handler = function (event, context) {
                  };
              });
              console.log("Zone IDs:");
-             console.log(zone_ids);
+             console.log(JSON.stringify(zone_ids, null, 2));
              next(null, route53Tags, resource_records, reverse_records, zone_ids);
          },
          function matchReverseIpsWithReverseIds(route53Tags, resource_records, reverse_records, zone_ids, next) {
@@ -154,7 +187,7 @@ exports.handler = function (event, context) {
          },
          function setupDNSReverseChanges(route53Tags, resource_records, reverse_map, next) {
              console.log("Reverse Map:");
-             console.log(reverse_map);
+             console.log(JSON.stringify(reverse_map, null, 2));
              for (var i = 0; i < Object.length; i++) {
                  var zone_id = Object.keys(reverse_map)[i];
                  var records = reverse_map[zone_id];
@@ -166,10 +199,16 @@ exports.handler = function (event, context) {
                  };
                  for (var j = 0; j < records.length; j++) {
                      var fqdn_record = "";
-                     if (route53Tags.DomainName !== undefined) {
-                         fqdn_record = route53Tags.RecordName + "-" + j + "." + route53Tags.DomainName;
+                     var cnt;
+                     if (j.length == 1) {
+                       cnt = "0" + j;
                      } else {
-                         fqdn_record = route53Tags.RecordName + "-" + j;
+                       cnt = j;
+                     }
+                     if (route53Tags.DomainName !== undefined) {
+                         fqdn_record = route53Tags.RecordName + "-" + cnt + "." + route53Tags.DomainName;
+                     } else {
+                         fqdn_record = route53Tags.RecordName + "-" + cnt;
                      }
                      zone_change.ChangeBatch.Changes.push({
                          Action: 'UPSERT',
@@ -181,9 +220,11 @@ exports.handler = function (event, context) {
                          }
                      });
                  }
-                 console.log("Updating Route53 Reverse DNS change request (fqdn_record)");
-                 console.log(zone_change);
-                 route53.changeResourceRecordSets(zone_change, next);
+                 if (!do_debug) {
+                   console.log("Updating Route53 Reverse DNS change request (" + fqdn_record + ")");
+                   console.log(JSON.stringify(zone_change, null, 2));
+                   route53.changeResourceRecordSets(zone_change, next);
+                 }
              }
              next(null, route53Tags, resource_records);
          },
@@ -210,10 +251,16 @@ exports.handler = function (event, context) {
                  }
              });
              for (var i = 0; i < resource_records.length; i++) {
-                 if (route53Tags.DomainName !== undefined) {
-                     record = route53Tags.RecordName + "-" + i + "." + route53Tags.DomainName;
+                 var cnt;
+                 if (i.length == 1) {
+                   cnt = "0" + i;
                  } else {
-                     record = route53Tags.RecordName + "-" + i;
+                   cnt = i;
+                 }
+                 if (route53Tags.DomainName !== undefined) {
+                     record = route53Tags.RecordName + "-" + cnt + "." + route53Tags.DomainName;
+                 } else {
+                     record = route53Tags.RecordName + "-" + cnt;
                  }
                  zone_change.ChangeBatch.Changes.push({
                      Action: 'UPSERT',
@@ -225,9 +272,12 @@ exports.handler = function (event, context) {
                      }
                  });
              };
-             console.log("Updating Route53 Forward DNS change request");
-             console.log(zone_change);
-             route53.changeResourceRecordSets(zone_change, next);
+             if (!do_debug) {
+               console.log("Updating Route53 Forward DNS change request");
+               console.log(JSON.stringify(zone_change, null, 2));
+               route53.changeResourceRecordSets(zone_change, next);
+             }
+             console.log("End of updateDNSForward()");
          }
     ], function (err) {
          if (err) {
@@ -237,8 +287,9 @@ exports.handler = function (event, context) {
          }
          context.done(err);
     });
+    console.log("End of if(asg_event)");
   } else {
-    console.log("Unsupported ASG event: " + asg_name, asg_event);
+    console.error("Unsupported ASG event: " + asg_name, asg_event);
     context.done("Unsupported ASG event: " + asg_name, asg_event);
   }
 };
